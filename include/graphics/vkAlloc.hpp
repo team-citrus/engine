fc5433f28510bea790799d422c64b8dd0544473e
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-11-14 19:06:28 -0500

    Added include/graphics/vkAlloc.hpp
    Added include/graphics/vkAlloc.hpp, a Vulkan internal header containing Vulkan memory allocation functions.

---
+++ include/graphics/vkAlloc.hpp
@@ -0,0 +1,26 @@
+#ifndef CITRUS_ENGINE_VULKAN_MEMORY_ALLOCATION_HPP__
+#define CITRUS_ENGINE_VULKAN_MEMORY_ALLOCATION_HPP__
+
+#include <vulkan.h>
+
+/* Vulkan memory allocation and deallocation */
+
+namespace engine
+{
+namespace internals
+{
+    namespace Vulkan
+    {
+        /* Allocate memory of size bytes, of type type */
+        void *malloc(size_t bytes, int type);
+        
+        /* Reallocate memory of size bytes, of type type */
+        void *realloc(void *alloc, size_t bytes, int type);
+
+        /* Free memory */
+        void free(void *alloc);
+    };
+};
+};
+
+#endif
