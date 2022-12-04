2f7ab42e2e44a3857455f4186a6d820d9a78a787
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-12-03 23:38:04 -0500

    Wrote a file for the functions relating to the transform methods of engine::physics::rigidbody and engine::physics::physobject
    Wrote src/physics/setTransform.cpp, a file for the functions relating to the transform methods of engine::physics::rigidbody and engine::physics::physobject.

---
+++ src/physics/setTransform.cpp
@@ -0,0 +1,159 @@
+#include <cstdlib>
+#include "include/core/extensions.hpp"
+#include "include/physics/vectors.hpp"
+#include "include/physics/physobject.hpp"
+#include "include/physics/rigidbody.hpp"
+
+using namespace engine;
+using namespace physics;
+
+OPERATOR void rigidbody::setPos(double x, double y)
+{
+    if(ownerSet)
+        owner->setPos(x, y);
+
+    return;
+}
+
+OPERATOR void rigidbody::setPos(double x, double y, double z)
+{
+    if(ownerSet)
+        owner->setPos(x, y, z);
+    return;
+}
+
+void rigidbody::setPos(vec2 pos)
+{
+    if(ownerSet)
+        owner->setPos(pos);
+    return;
+}
+
+void rigidbody::setPos(vec3 pos)
+{
+    if(ownerSet)
+        owner->setPos(pos);
+    return;
+}
+
+OPERATOR void physobject::setPos(double x, double y)
+{
+    setPos(vec2(x, y));
+    return;
+}
+
+OPERATOR void physobject::setPos(double x, double y, double z)
+{
+    setPos(vec3(x, y, z));
+    return;
+}
+
+void physobject::setPos(vec2 pos)
+{
+    coordinates2D = pos;
+}
+
+void physobject::setPos(vec3 pos)
+{
+    coordinates3D = pos;
+}
+
+OPERATOR void rigidbody::setRot(double x, double y)
+{
+    if(ownerSet)
+        owner->setRot(x, y);
+    return;
+}
+
+OPERATOR void rigidbody::setRot(double x, double y, double z)
+{
+    if(ownerSet)
+        owner->setRot(x, y, z);
+    return;
+}
+
+void rigidbody::setRot(vec2 rot)
+{
+    if(ownerSet)
+        owner->setRot(rot);
+    return;
+}
+
+void rigidbody::setRot(vec3 rot)
+{
+    if(ownerSet)
+        owner->setRot(rot);
+    return;
+}
+
+OPERATOR void physobject::setRot(double x, double y)
+{
+    setRot(vec2(x, y));
+    return;
+}
+
+OPERATOR void physobject::setRot(double x, double y, double z)
+{
+    setRot(vec3(x, y, z));
+    return;
+}
+
+void physobject::setRot(vec2 rot)
+{
+    rotation2D = rot;
+}
+
+void physobject::setRot(vec3 rot)
+{
+    rotation3D = rot;
+}
+
+OPERATOR void rigidbody::setScale(double x, double y)
+{
+    if(ownerSet)
+        owner->setScale(x, y);
+    return;
+}
+
+OPERATOR void rigidbody::setScale(double x, double y, double z)
+{
+    if(ownerSet)
+        owner->setScale(x, y, z);
+    return;
+}
+
+void rigidbody::setScale(vec2 scale)
+{
+    if(ownerSet)
+        owner->setScale(scale);
+    return;
+}
+
+void rigidbody::setScale(vec3 scale)
+{
+    if(ownerSet)
+        owner->setScale(scale);
+    return;
+}
+
+OPERATOR void physobject::setScale(double x, double y)
+{
+    setScale(vec2(x, y));
+    return;
+}
+
+OPERATOR void physobject::setScale(double x, double y, double z)
+{
+    setScale(vec3(x, y, z));
+    return;
+}
+
+void physobject::setScale(vec2 scale)
+{
+    coordinates2D = scale;
+}
+
+void physobject::setScale(vec3 scale)
+{
+    coordinates3D = scale;
+}
