301369666701915910bc33f392504171badd74ea
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-01-13 19:29:05 -0500

    Changed the EXECUTE_ITEM_IN_QUEUE to be template based
    Changed the macro, EXECUTE_ITEM_IN_QUEUE, into a varadic template based inline function, executeItemInQueue().

--- include/core/workQueue.hpp
+++ include/core/workQueue.hpp
@@ -19,7 +19,11 @@ namespace engine
 		typedef size_t (*queuePtr)(...);
 		typedef Vector<queuePtr> workQueue;
 		
-		#define EXECUTE_ITEM_IN_QUEUE(x, ...) (__VA_ARGS_FIX__((queuePtr)x(__VA_ARGS__)))
+		template <typename...A>
+		OPERATOR size_t executeItemInQueue(queuePtr ptr, A...args)
+		{
+			return ptr(args...);
+		}
 
 		/*	Executes all items in a queue, applying all args passed to each of the items in the queue
 		*	@return Vector of each of the returns
@@ -29,7 +33,7 @@ namespace engine
 		{
 			Vector<size_t> ret;
 			for(int i = 0; i < queue.getCount(); i++)
-				ret.push(EXECUTE_ITEM_IN_QUEUE(queue[i], args...));
+				ret.push(executeItemInQueue(queue[i], args...));
 			return ret;
 		}
 	};
