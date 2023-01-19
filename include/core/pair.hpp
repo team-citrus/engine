884f8ccdcc1e03ec2b349f73aa741490e69d72ce
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-01-19 14:32:10 +0000

    Wrote a class for scenes
    Wrote a class and header for scene internals. Wrote include/core/pair.hpp, a header for pairs and maps. Wrote a header for the scene API.

---
+++ include/core/pair.hpp
@@ -0,0 +1,84 @@
+/*
+*   name: include/core/pair.hpp
+*   origin: Citrus Engine
+*   purpose: Provide pairs and maps
+*   author: https://github.com/ComradeYellowCitrusFruit
+*   license: LGPL-3.0-only
+*/
+
+#ifndef CITRUS_ENGINE_PAIRS_HPP___
+#define CITRUS_ENGINE_PAIRS_HPP___
+
+#include "include/core/vector.hpp"
+
+namespace engine
+{
+    template <typename A, typename B>
+    class pair
+    {
+        public:
+        pair(A aa, B bb)
+        {
+            a = aa;
+            b = bb;
+        }
+        A a;
+        B b;
+    };
+
+    template <typename A, typename B>
+    class map
+    {
+        public:
+        OPERATOR void addPair(A a, B b)
+        {
+            vec.push(pair<A, B>(a, b));
+        }
+
+        OPERATOR void addPair(pair<A, B> p)
+        {
+            vec.push(p);
+        }
+
+        OPERATOR pair<A, B> &getPair(A a)
+        {
+            for(int i = 0; i < vec.getCount(); i++)
+            {
+                if(vec[i].a == a)
+                    return vec[i];
+            }
+        }
+
+        OPERATOR pair<A, B> &getPair(B b)
+        {
+            for(int i = 0; i < vec.getCount(); i++)
+            {
+                if(vec[i].b == b)
+                    return vec[i];
+            }
+        }
+
+        OPERATOR B &getB(A a)
+        {
+            for(int i = 0; i < vec.getCount(); i++)
+            {
+                if(vec[i].a == a)
+                    return vec[i].b;
+            }
+        }
+
+        OPERATOR A &getA(B b)
+        {
+            for(int i = 0; i < vec.getCount(); i++)
+            {
+                if(vec[i].b == b)
+                    return vec[i].a;
+            }
+        }
+
+        private:
+        Vector<pair<A, B>> vec;
+    };
+};
+
+#endif
---
+++ include/core/scene_int.hpp
@@ -0,0 +1,38 @@
+/*
+*   name: include/core/scene_int.hpp
+*   origin: Citrus Engine
+*   purpose: Provide scene internals
+*   author: https://github.com/ComradeYellowCitrusFruit
+*   license: LGPL-3.0-only
+*/
+
+#ifndef CITRUS_ENGINE_SCENE_INTERNALS_HPP__
+#define CITRUS_ENGINE_SCENE_INTERNALS_HPP__
+
+#include "include/core/vector.hpp"
+#include "include/core/object.hpp"
+#include "include/core/pair.hpp"
+
+namespace engine
+{
+	namespace internals
+	{
+		typedef void (*sceneConstructorPtr_t(void));
+		class scene
+		{
+			public:
+			int ID;
+			// Scenes have special constructors generated to initalize everything in a scene.
+			sceneConstructorPtr_t constructor;
+			// TODO: Add a type for shaders
+			// All objects in a scene
+			Vector<object> objects;
+			bool is2D;
+		};
+		
+		extern scene *mainScene;
+		extern map<scene, int> scenes;
+	};
+};
+
+#endif
