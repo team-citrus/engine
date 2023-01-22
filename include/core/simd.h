612d7f95eb6ced4eeba0b3b594882999ba64def2
Teresa Lastname <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-01-22 15:13:40 -0500

    Added several more defines to include/core/simd.h

--- include/core/simd.h
+++ include/core/simd.h
@@ -39,6 +39,50 @@ typedef __m256i m256i_t;
 #define addsub_ps(a, b) _mm_addsub_ps(a, b)
 #define addsub_sd(a, b) _mm_addsub_sd(a, b)
 
+#define alignr_epi8(a, b, i) _mm_alignr_epi8(a, b, i)
+#define alignr_pi8(a, b, i) _mm_alignr_pi8(a, b, i)
+
+#define and_pd(a, b) _mm_and_pd(a, b)
+#define and_ps(a, b) _mm_and_ps(a, b)
+#define and_si128(a, b) _mm_and_si128(a, b)
+
+#define andnot_pd(a, b) _mm_andnot_pd(a, b)
+#define andnot_ps(a, b) _mm_andnot_ps(a, b)
+#define andnot_si128(a, b) _mm_andnot_si128(a, b)
+
+#define avg_epu8(a, b) _mm_avg_epu8(a, b)
+#define avg_epu16(a, b) _mm_avg_epu16(a, b)
+
+#define avg_pu8(a, b) _mm_avg_pu8(a, b)
+#define avg_pu16(a, b) _mm_avg_pu16(a, b)
+
+#define blend_epi16(a, b, i) _mm_blend_epi16(a, b, i)
+#define blend_pd(a, b, i) _mm_blend_pd(a, b, i)
+#define blend_ps(a, b, i) _mm_blend_ps(a, b, i)
+
+#define blendv_epi8(a, b, m) _mm_blendv_epi8(a, b, m)
+#define blendv_pd(a, b, m) _mm_blendv_pd(a, b, m)
+#define blendv_ps(a, b, m) _mm_blendv_ps(a, b, m)
+
+#define bslli_si128(a, i) _mm_bslli_si128(a, i)
+#define bsrli_si128(a, i) _mm_bsrli_si128(a, i)
+
+#define castpd_ps(a) _mm_castpd_ps(a)
+#define castpd_si128(a) _mm_castpd_si128(a)
+
+#define castps_pd(a) _mm_castps_pd(a)
+#define castps_si128(a) _mm_castps_si128(a)
+
+#define castsi128_pd(a) _mm_castsi128_pd(a)
+#define castsi128_ps(a) _mm_castsi128_ps(a)
+
+#define ceil_pd(a) _mm_ceil_pd(a)
+#define ceil_ps(a) _mm_ceil_ps(a)
+#define ceil_sd(a, b) _mm_ceil_sd(a, b)
+#define ceil_ss(a, b) _mm_ceil_ss(a, b)
+
+#define clflush(cvp) _mm_cflush(cvp)
+
 // TODO: Add more stuff
 
 #endif
