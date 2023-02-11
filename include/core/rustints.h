6153110f1f5e62f14632445207636b908d5ca271
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-02-10 20:50:19 -0500

    Added some utility functions to rustints.h
    Added some utility functions such as max(), min(), and abs() to include/core/rustints.h

--- include/core/rustints.h
+++ include/core/rustints.h
@@ -1,10 +1,10 @@
 /*
-*   name: include/core/rustints.h
-*   origin: Citrus Engine
-*   purpose: Provide Rust style int types
-*   author: https://github.com/ComradeYellowCitrusFruit
-*   license: CC0-1.0 OR LGPL-3.0-ONLY
-*/
+ *   name: include/core/rustints.h
+ *   origin: Citrus Engine
+ *   purpose: Provide Rust style int types
+ *   author: https://github.com/ComradeYellowCitrusFruit
+ *   license: CC0-1.0 OR LGPL-3.0-ONLY
+ */
 
 #ifndef CITRUS_ENGINE_RUST_INTEGER_TYPES_H__
 #define CITRUS_ENGINE_RUST_INTEGER_TYPES_H__
@@ -47,5 +47,87 @@ typedef double f64;
 #define U16_MIN (u16)0
 #define U32_MIN (u32)0
 #define U64_MIN (u64)0
+
+#ifdef __cplusplus
+
+namespace engine
+{
+
+    template <typename T>
+    T max(T a, T b)
+    {
+        return (a > b) ? a : b;
+    }
+
+    template <typename T>
+    T min(T a, T b)
+    {
+        return (a < b) ? a : b;
+    }
+
+    template <typename T>
+    T abs(T a);
+
+    u8 abs(u8 a)
+    {
+        return a;
+    }
+
+    u16 abs(u16 a)
+    {
+        return a;
+    }
+
+    u32 abs(u32 a)
+    {
+        return a;
+    }
+
+    u64 abs(u64 a)
+    {
+        return a;
+    }
+
+    i8 abs(i8 a)
+    {
+        return (a & 0x80) ? ~a + 1 : a;
+    }
+
+    i16 abs(i16 a)
+    {
+        return (a & 0x8000) ? ~a + 1 : a;
+    }
+
+    i32 abs(i32 a)
+    {
+        return (a & 0x80000000) ? ~a + 1 : a;
+    }
+
+    i64 abs(i64 a)
+    {
+        return (a & 0x8000000000000000) ? ~a + 1 : a;
+    }
+
+    f32 abs(f32 a)
+    {
+        return (a < 0.0f) ? a * -1.0f : a;
+    }
+
+    f64 abs(f64 a)
+    {
+        return (a < 0.0) ? a * -1.0 : a;
+    }
+
+    template <typename T>
+    T pow(T a, int power)
+    {
+        return (power) ? a * pow(a, power - 1) : (T)1;
+    }
+
+    // TODO: sqrt and isqrt
+
+}
+
+#endif
 
 #endif
