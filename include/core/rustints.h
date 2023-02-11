76dd431561673c5fbd247189c3856560e465fc2c
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-02-10 20:07:47 -0500

    Added *_MAX and *_MIN defines
    Added *_MAX and *_MIN defines for all integer types in include/core/rustints.h

--- include/core/rustints.h
+++ include/core/rustints.h
@@ -28,5 +28,24 @@ typedef long long isize; // ssize_t is only defined in *nix, however we can assu
 typedef float f32;
 typedef double f64;
 
+#define I8_MAX (i8)0x7F
+#define I16_MAX (i16)0x7FFF
+#define I32_MAX (i32)0x7FFFFFFF
+#define I64_MAX (i64)0x7FFFFFFFFFFFFFFF
+
+#define I8_MIN (i8)0x80
+#define I16_MIN (i16)0x8000
+#define I32_MIN (i32)0x80000000
+#define I64_MIN (i64)0x8000000000000000
+
+#define U8_MAX (u8)0xFF
+#define U16_MAX (u16)0xFFFF
+#define U32_MAX (u32)0xFFFFFFFF
+#define U64_MAX (u64)0xFFFFFFFFFFFFFFFF
+
+#define U8_MIN (u8)0
+#define U16_MIN (u16)0
+#define U32_MIN (u32)0
+#define U64_MIN (u64)0
 
 #endif
