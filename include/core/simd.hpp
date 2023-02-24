2cf4ee12a409d1f9a376ec378938e7f57b3b0dd8
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-02-24 10:00:41 -0500

    Added more operators to m128_i8
    Added more operators to m128_i8, including subtraction, and bitwise operations.

--- include/core/simd.hpp
+++ include/core/simd.hpp
@@ -16,8 +16,8 @@
 
 class m128_i8
 {
-    m128i_t Pi8;
     public:
+    m128i_t Pi8;
 
     m128_i8(m128i_t i)
     {
@@ -34,7 +34,87 @@ class m128_i8
         return m128_i8(add_epi8(Pi8, i));
     }
 
-    m128_i8 operator+(i8 i);
+    m128_i8 operator+(i8 i)
+    {
+        i8 arr[16] = { i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i };
+        m128i_t a = movu_s128(arr);
+        return *this + m128_i8(a);
+    }
+    
+    m128_i8 operator-(m128_i8 &i)
+    {
+        return m128_i8(sub_epi8(Pi8, i.Pi8));
+    }
+
+    m128_i8 operator-(m128i_t &i)
+    {
+        return m128_i8(sub_epi8(Pi8, i));
+    }
+
+    m128_i8 operator-(i8 i)
+    {
+        i8 arr[16] = { i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i };
+        m128i_t a = movu_s128(arr);
+        return *this - m128_i8(a);
+    }
+    
+    m128_i8 operator|(m128_i8 &i)
+    {
+        return m128_i8(or_si128(Pi8, i.Pi8));
+    }
+
+    m128_i8 operator|(m128i_t &i)
+    {
+        return m128_i8(or_si128(Pi8, i));
+    }
+
+    m128_i8 operator|(i8 i)
+    {
+        i8 arr[16] = { i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i };
+        m128i_t a = movu_s128(arr);
+        return *this | m128_i8(a);
+    }
+    
+    m128_i8 operator&(m128_i8 &i)
+    {
+        return m128_i8(and_si128(Pi8, i.Pi8));
+    }
+
+    m128_i8 operator&(m128i_t &i)
+    {
+        return m128_i8(and_si128(Pi8, i));
+    }
+
+    m128_i8 operator&(i8 i)
+    {
+        i8 arr[16] = { i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i };
+        m128i_t a = movu_s128(arr);
+        return *this & m128_i8(a);
+    }
+    
+    m128_i8 operator|(i8 i)
+    {
+        i8 arr[16] = { i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i };
+        m128i_t a = movu_s128(arr);
+        return *this | m128_i8(a);
+    }
+    
+    m128_i8 operator^(m128_i8 &i)
+    {
+        return m128_i8(xor_si128(Pi8, i.Pi8));
+    }
+
+    m128_i8 operator^(m128i_t &i)
+    {
+        return m128_i8(xor_si128(Pi8, i));
+    }
+
+    m128_i8 operator^(i8 i)
+    {
+        i8 arr[16] = { i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i };
+        m128i_t a = movu_s128(arr);
+        return *this ^ m128_i8(a);
+    }
 };
 
 #endif
