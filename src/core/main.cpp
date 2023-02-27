263a979f4e36002fdd7d9f993b269433b29041c4
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-02-27 00:23:44 -0500

    Added signal handlers
    Added signal handlers, located in main.cpp.

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -24,6 +24,9 @@
 #include <ctime>
 #include <csetjmp>
 #include <cstdlib>
+#include <csignal>
+#include <cstdio>
+#include <cpuid.h>
 #include "core/mem.hpp"
 #include "core/mem_int.hpp"
 #include "core/Main.hpp"
@@ -31,6 +34,8 @@
 #include "core/scene_int.hpp"
 #include "core/workQueue.hpp"
 #include "core/log.hpp"
+#include "core/errno.hpp"
+#include "core/crash.hpp"
 #include "graphics/vkInit.hpp"
 #include "graphics/initGL.hpp"
 
@@ -64,6 +69,137 @@ void waitms(size_t mils)
     #else 
 
     Sleep(mils); // Rare instance of the Windows version being easier than the POSIX version
+
+    #endif
+}
+
+#ifndef _WIN32
+
+#define SIGARGS int sig, siginfo_t *info, void *ucontext
+
+#else
+
+#define SIGARGS int sig
+
+#endif
+
+void sigsev(SIGARGS)
+{
+    #ifndef _WIN32
+
+    #ifdef _DEBUG_
+
+    engine::log(STRINGIFY(Segfault handler), "Segfault triggered, dumping memory pool.");
+    engine::log(STRINGIFY(Segfault handler), "Memory pool size: %zu", engine::internals::pool.size);
+    engine::log(STRINGIFY(Segfault handler), "Pool limit: %zu", engine::internals::pool.limit);
+    engine::log(STRINGIFY(Segfault handler), "Limit exceeded: %p", engine::internals::pool.limitExceeded);
+    engine::log(STRINGIFY(Segfault handler), "Offending address: %p", info->si_addr);
+    engine::log(STRINGIFY(Segfault handler), "Dumping core now.");
+
+    FILE *poolf = fopen("poolclass", "w");
+    FILE *core = fopen("core", "wb");
+
+    fwrite(&engine::internals::pool, sizeof(engine::internals::Pool), 1, poolf);
+    fwrite(engine::internals::start, 1, engine::internals::pool.size, core);
+
+    engine::log(STRINGIFY(Segfault handler), "Core dump complete.");
+
+    exit(SEGFAULT);
+
+    #else
+
+    engine::log(STRINGIFY(Segfault handler), "Segmentation fault!");
+    exit(SEGFAULT);
+
+    #else
+
+    #ifdef _DEBUG_
+
+    engine::log(STRINGIFY(Segfault handler), "Segfault triggered, dumping memory pool.");
+    engine::log(STRINGIFY(Segfault handler), "Memory pool size: %zu", engine::internals::pool.size);
+    engine::log(STRINGIFY(Segfault handler), "Pool limit: %zu", engine::internals::pool.limit);
+    engine::log(STRINGIFY(Segfault handler), "Limit exceeded: %p", engine::internals::pool.limitExceeded);
+    engine::log(STRINGIFY(Segfault handler), "Offending address unknown.");
+    engine::log(STRINGIFY(Segfault handler), "Dumping core now.");
+
+    FILE *poolf = fopen("poolclass", "w");
+    FILE *core = fopen("core", "wb");
+
+    fwrite(&engine::internals::pool, sizeof(engine::internals::Pool), 1, poolf);
+    fwrite(engine::internals::start, 1, engine::internals::pool.size, core);
+
+    engine::log(STRINGIFY(Segfault handler), "Core dump complete.");
+
+    exit(SEGFAULT);
+
+    #else
+
+    engine::log(STRINGIFY(Segfault handler), "Segmentation fault!");
+    exit(SEGFAULT);
+
+    #endif
+}
+
+void sigill(SIGARGS)
+{
+    engine::log(STRINGIFY(Illegal instruction handler), "An illegal instruction has been triggered!");
+    #if defined(_DEBUG_) && !defined(_WIN32)
+
+    engine::log(STRINGIFY(Illegal instruction handler), "Offending address: %p", info->si_addr);
+
+    #endif
+    
+    if(!__builtin_cpu_supports("sse4.2")
+        #ifdef _MAVX_
+        
+        || !__builtin_cpu_supports("avx")
+        
+        #if _MAVX_ == 2
+
+        || !!__builtin_cpu_supports("avx2")
+        
+        #endif
+        
+        #endif
+    ) // TODO: Turn those checks into a function
+    {
+        engine::log(STRINGIFY(Illegal instruction handler), "CPU does not support necessary instruction sets!");
+
+        // TODO: Dump CPUID
+        // TODO: Dump build flags
+
+        exit(INVALID_CPU);
+    }
+    else
+    {
+        exit(SIG_ILLEGAL_INSTRUCTION);
+    }
+}
+
+// Initialize signals, very different depending on build types and operating systems
+static inline void initSigs()
+{
+    #ifndef _WIN32
+
+    sigaction ssigsev;
+    sigaction ssigill;
+    ssigsev.sa_sigaction = sigsev;
+    ssigsev.sa_mask = 0;
+    ssigsev.sa_flags = SA_SIGINFO;
+    sigaction(SIGSEGV, &ssigsev, NULL);
+    ssigill.sa_sigaction = sigill;
+    ssigill.sa_mask = 0;
+    ssigill.sa_flags = SA_SIGINFO;
+    sigaction(SIGILL, &ssigill, NULL);
+
+    // TODO: Other signals
+
+    #else
+
+    signal(SIGSEGV, sigsev);
+    signal(SIGILL, sigsev);
+
+    // TODO: Other signals
 
     #endif
 }
@@ -116,6 +252,7 @@ int MAIN
     
     initLogging();
     pool = Pool();
+    initSigs();
 
     // TODO: Implement settings
 
