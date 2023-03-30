ff67ddf361dccf3061b1873cbf70ecd6e02b4256
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-03-30 17:26:04 +0000

    Implemented AES-CTR-256 for RNG
    Implemented AES-CTR-256 for random number generation on x64 machines.

--- include/core/rng.hpp
+++ include/core/rng.hpp
@@ -15,6 +15,11 @@
 #include "core/rustints.h"
 
 #define ROTL(x, b) (x << b) | (x >> (sizeof(x)*8 - b))
+#ifndef __x86_64__
+#define BYTES_MOD 64
+#else
+#define BYTES_MOD 16
+#endif
 
 namespace engine
 {
@@ -22,17 +27,29 @@ namespace engine
     {
         void initMainRNG();
         void getSeedBytes(uint32_t matrix[]);
+
+        #ifdef __x86_64__
+        void AES(uint8_t key[], uint64_t unique[], uint8_t output[]);
+        #endif
     }
 
     class RandomNumberGenerator
     {
         union
         {
-            uint32_t matrix[16];
-            uint8_t bytes[64];
+            uint32_t matrix[BYTES_MOD/4];
+            uint8_t bytes[BYTES_MOD];
         };
         
-        int ctr;
+        uint64_t ctr;
+
+        #ifdef __x86_64__
+
+        uint64_t nonce;
+
+        #endif
+
+        #ifndef __x86_64__
 
         void QR(int a, int b, int c, int d)
         {
@@ -43,8 +60,11 @@ namespace engine
             matrix[c] += matrix[d]; matrix[b] ^= matrix[c]; matrix[b] = ROTL(matrix[d], 7);
         }
 
+        #endif
+
         void shuffle()
         {
+            #ifndef __x86_64__
             uint32_t omatrix[16];
             memcpy(omatrix, matrix, 64);
             for(int i = 0; i < 10; i++)
@@ -55,11 +75,9 @@ namespace engine
 		        QR(3, 7, 11, 15);
 		        QR(0, 5, 10, 15);
 		        QR(1, 6, 11, 12);
-		        QR(2, 7, 8, 13]);
-		        QR(3, 4, 9], 14);
+		        QR(2, 7, 8, 13);
+		        QR(3, 4, 9, 14);
             }
-
-            #ifndef __x86_64__
 
             // Use Neon on non-AMD64 systems
             uint32x4_t *nmatrix = &matrix[0];
@@ -68,25 +86,17 @@ namespace engine
             for(int i = 0; i < 4; i++)
                 vst1q_u32((uint32_t*)nmatrix + (i * 4), vaddq_u32(vld1q_u32((uint32_t*)nmatrix + (i * 4), vld1q_u32((uint32_t*)iomatrix + (i * 4)))));
             
-            #elif _MAVX_ >= 512
-
-            _mm512_storeu_si512(&matrix[0], _mm512_add_epi32(_mm512_loadu_si512(&matrix[0]), _mm512_loadu_si512(&omatrix[0])));
-
-            #elif _MAVX_ == 2
-
-            m256i_t *nmatrix = &matrix[0];
-            m256i_t *iomatrix = &omatrix[0];
-
-            ustore_i256(nmatrix, add256_i32(uload_i256(nmatrix), uload_i256(iomatrix)));
-            ustore_i256(nmatrix + 1, add256_i32(uload_i256(nmatrix + 1), uload_i256(iomatrix + 1)));
-
             #else
 
-            m128i_t *nmatrix = &matrix[0];
-            m128i_t *iomatrix = &omatrix[0];
+            // TODO: Use SHA256 to generate the required key
 
-            for(int i = 0; i < 4; i++)
-                store_i128(nmatrix + i, add_i32(load_i128(nmatrix + i), load_i128(iomatrix + i)));
+            uint8_t key[32];
+            xmm_memcpy(key, bytes, 1);
+            for(int i = 16; i < 32; i += 8)
+                _rdseed64_step((unsigned long long*)(key + i)); // Fuck, where else will we get more random numbers?
+
+            uint64_t unique[2] = {ctr / 16, nonce};
+            internals::AES(key, unique, bytes);
 
             #endif
         }
@@ -98,6 +108,7 @@ namespace engine
 
         RandomNumberGenerator()
         {
+            #ifndef __x86_64__
             internals::getSeedBytes(matrix);
 
             matrix[0] = 0x61707865;
@@ -106,6 +117,13 @@ namespace engine
             matrix[3] = 0x6b206574;
             matrix[12] = 0;
             ctr = 0;
+
+            #else
+
+            internals::getSeedBytes((uint32_t*)this);
+            ctr = 0;
+
+            #endif
         }
 
         u64 genU64()
@@ -113,13 +131,13 @@ namespace engine
             uint64_t ret = 0;
             for(int i = 0; i < 8; i++)
             {
-                if(ctr % 64 == 0)
+                if(ctr % BYTES_MOD == 0)
                 {
                     shuffle();
                 }
 
                 ret <<= 8;
-                ret |= bytes[ctr % 64];
+                ret |= bytes[ctr % BYTES_MOD];
                 ctr++;
             }
             
@@ -131,13 +149,13 @@ namespace engine
             uint32_t ret = 0;
             for(int i = 0; i < 4; i++)
             {
-                if(ctr % 64 == 0)
+                if(ctr % BYTES_MOD == 0)
                 {
                     shuffle();
                 }
 
                 ret <<= 8;
-                ret |= bytes[ctr % 64];
+                ret |= bytes[ctr % BYTES_MOD];
                 ctr++;
             }
         }
@@ -146,24 +164,24 @@ namespace engine
             uint16_t ret = 0;
             for(int i = 0; i < 2; i++)
             {
-                if(ctr % 64 == 0)
+                if(ctr % BYTES_MOD == 0)
                 {
                     shuffle();
                 }
 
                 ret <<= 8;
-                ret |= bytes[ctr % 64];
+                ret |= bytes[ctr % BYTES_MOD];
                 ctr++;
             }
         }
         u8 genU8()
         {
-            if(ctr % 64 == 0)
+            if(ctr % BYTES_MOD == 0)
             {
                 shuffle();
             }
 
-            return bytes[ctr++ % 64];
+            return bytes[ctr++ % BYTES_MOD];
         }
         
         void genBytes(uint8_t *buffer, size_t len)
@@ -172,12 +190,12 @@ namespace engine
 
             for(int i = 0; i < len; i++)
             {
-                if(ctr % 64 == 0)
+                if(ctr % BYTES_MOD == 0)
                 {
                     shuffle();
                 }
 
-                buffer[i] = bytes[ctr++ % 64];
+                buffer[i] = bytes[ctr++ % BYTES_MOD];
             }
         }
 
--- src/core/rng.cpp
+++ src/core/rng.cpp
@@ -19,6 +19,40 @@
 
 engine::RNG mainRNG ALIGN(64);
 
+#ifdef __x86_64__
+
+ALWAYS_INLINE uint8_t rcon(int round)
+{
+    if(round <= 1) return 1;
+    else (round<<1) ^ (0x11b & -(round>>7));
+}
+
+ALWAYS_INLINE void roundKey(uint8_t key[], uint8_t rkey[], int round)
+{
+    uint32_t *K = key;
+    uint32_t *W = rkey;
+
+    if(round * 4 < 8)
+    {
+        if((uintptr_t)K & 0xF && (uintptr_t)W & 0xF)
+        {
+            xmm_memcpy(W, K + (round * 4), 1);
+        }
+        else
+        {
+            ustore_i128(W, uload_i128(K + (round * 4)));
+        }
+    }
+    else
+    {
+        // That's how it works right? We'll have to check later
+        ustore_i128((m128i_t*)W, _mm_aeskeygenassist_si128(uload_i128(K + ((round & 1) * 4)), rcon(round)));
+    }
+}
+
+#endif
+
+
 void engine::internals::initMainRNG()
 {
     #ifndef _WIN32
@@ -33,18 +67,62 @@ void engine::internals::initMainRNG()
 
     #endif
 
+    #ifndef __x86_64__
+
     mainRNG.matrix[0] = 0x61707865;
     mainRNG.matrix[1] = 0x3320646e;
     mainRNG.matrix[2] = 0x79622d32;
     mainRNG.matrix[3] = 0x6b206574;
     mainRNG.matrix[12] = 0;
     mainRNG.ctr = 0;
+
+    #endif
 }
 
 void engine::internals::getSeedBytes(uint32_t matrix[])
 {
-    if((uintptr_t)matrix & 0x3F) zmm_memcpy(matrix, mainRNG.matrix, 64);
-    else if((uintptr_t)matrix & 0x1F) ymm_memcpy(matrix, mainRNG.matrix, 64);
-    else xmm_memcpy(matrix, mainRNG.matrix, 64);
+    #ifndef __x86_64__
+
+    if((uintptr_t)matrix & 0x3F) zmm_memcpy(matrix, mainRNG.matrix, 1);
+    else if((uintptr_t)matrix & 0x1F) ymm_memcpy(matrix, mainRNG.matrix, 2);
+    else xmm_memcpy(matrix, mainRNG.matrix, 4);
     mainRNG.shuffle();
+
+    #else
+
+    if((uintptr_t)matrix & 0x1F) ymm_memcpy(matrix, &mainRNG, sizeof(mainRNG)/32);
+    else xmm_memcpy(matrix, &mainRNG, sizeof(mainRNG)/16);
+    mainRNG.ctr += 16;
+    mainRNG.shuffle();
+
+    #endif
 }
+
+#ifdef __x86_64__
+
+void engine::internals::AES(uint8_t key[], uint64_t unique[], uint8_t output[])
+{
+    ustore_i128(output, uload_i128(unique));
+    for(int i = 0; i < 14; i++)
+    {
+        uint8_t keybuff[16];
+        roundKey(key, keybuff, i);
+        
+        switch(i)
+        {
+            case 13:
+
+            ustore_i128(output, _mm_aesenclast_si128(uload_i128(output), uload_i128(keybuff)));
+
+            break;
+
+            default:
+
+            ustore_i128(output, _mm_aesenc_si128(uload_i128(output), uload_i128(keybuff)));
+
+            break;
+        }
+    }
+}
+
+#endif
