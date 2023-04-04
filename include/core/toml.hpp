428c57bbe1f128f9647b2e44ac2a6587c85bb386
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-04-04 07:40:49 -0400

    Rewrote toml.hpp
    Rewrote toml.hpp to work better with the recent changes.

--- include/core/toml.hpp
+++ include/core/toml.hpp
@@ -6,16 +6,9 @@
 *	license: LGPL-3.0-only
 */
 
-#include <toml.hpp>
-#include "core/extensions.h"
-
 #ifndef CITRUS_ENGINE_TOML_PARSER_HPP__
 #define CITRUS_ENGINE_TOML_PARSER_HPP__
 
-namespace engine
-{
-	using toml = engine::internals::toml;
-	using namespace engine::toml;
-}
+#include <toml.hpp>
 
 #endif
