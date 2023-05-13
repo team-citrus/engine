7588daf74938da38c0f71dbee21bc6b22b9cc0eb
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-05-12 21:57:54 -0400

    Added constructors to Transform2D and Transform3D
    Added constructors to Transform2D and Transform3D, as well as the assignment and comparison operators.

--- include/core/transform.hpp
+++ include/core/transform.hpp
@@ -1,10 +1,10 @@
 /*
-*   name: include/core/transform.hpp
-*   origin: Citrus Engine
-*   purpose: Provide engine::transform2D, and engine::transform3D
-*   author: https://github.com/ComradeYellowCitrusFruit
-*   license: LGPL-3.0-only
-*/
+ *   name: include/core/transform.hpp
+ *   origin: Citrus Engine
+ *   purpose: Provide engine::transform2D, and engine::transform3D
+ *   author: https://github.com/ComradeYellowCitrusFruit
+ *   license: LGPL-3.0-only
+ */
 
 #ifndef CITRUS_ENGINE_TRANSFORM_HPP__
 #define CITRUS_ENGINE_TRANSFORM_HPP__
@@ -18,106 +18,151 @@ namespace engine
 	using Float2 = physics::Vec2;
 	using Float3 = physics::Vec3;
 	using Float4 = physics::Vec4;
+	using Quaternion = physics::Quaternion;
+	using Quat = physics::Quat;
 
 	class Transform2D : Component
 	{
-		public:
-			Float2 position;
+	public:
+		Float2 position;
 
-			float rotation;
+		float rotation;
 
-			Float2 scale;
+		Float2 scale;
 
-			Transform2D()
-			{
-				position = Float2(0, 0);
-				rotation = 0;
-				scale = Float2(1, 1);
-			}
+		Transform2D()
+		{
+			position = Float2(0, 0);
+			rotation = 0;
+			scale = Float2(1, 1);
+		}
 
-			Transform2D(Float2 p)
-			{
-				position = p;
-				rotation = 0;
-				scale = Float2(1, 1);
-			}
+		Transform2D(Float2 p)
+		{
+			position = p;
+			rotation = 0;
+			scale = Float2(1, 1);
+		}
 
-			Transform2D(Float2 p, float r)
-			{
-				position = position;
-				rotation = r;
-				scale = Float2(1, 1);
-			}
+		Transform2D(Float2 p, float r)
+		{
+			position = position;
+			rotation = r;
+			scale = Float2(1, 1);
+		}
 
-			virtual void awake()
-			{
-				return;
-			}
-			virtual void start()
-			{
-				return;
-			}
-			virtual void update()
-			{
-				return;
-			}
-			
-			virtual void fixedUpdate()
-			{
-				return;
-			}
-			virtual void onTriggerEnter()
-			{
-				return;
-			}
-			virtual void onTriggerStay()
-			{
-				return;
-			}
-			virtual void onTriggerExit()
-			{
-				return;
-			}
+		Transform2D(Float2 p, float r, Float s)
+		{
+			position = p;
+			rotation = r;
+			scale = s;
+		}
+
+		OPERATOR Transform2D &operator=(Transform2D &otra)
+		{
+			position = otra.position;
+			rotation = otra.rotation;
+			scale = otra.scale;
+		}
+
+		OPERATOR bool operator==(Transform2D &otra)
+		{
+			return (position == otra.position) && (rotation == otra.rotation) && (scale == otra.scale);
+		}
 	}
 
 	class Transform3D : Component
 	{
-		public:
-			Float3 position;
+	public:
+		Float3 position;
+		Quaternion rotation;
+		Float3 scale;
 
-			Float3 rotation;
+		Transform3D()
+		{
+			position = Float3(0, 0, 0);
+			rotation = Quaternion(Float3(0, 0, 0));
+			scale = Float3(1, 1, 1);
+		}
 
-			Float3 scale;
+		Transform3D(Float3 p)
+		{
+			position = p;
+			rotation = Quaternion(Float3(0, 0, 0));
+			scale = Float3(1, 1, 1);
+		}
 
-			virtual void awake()
-			{
-				return;
-			}
-			virtual void start()
-			{
-				return;
-			}
-			virtual void update()
-			{
-				return;
-			}
-			
-			virtual void fixedUpdate()
-			{
-				return;
-			}
-			virtual void onTriggerEnter()
-			{
-				return;
-			}
-			virtual void onTriggerStay()
-			{
-				return;
-			}
-			virtual void onTriggerExit()
-			{
-				return;
-			}
+		Transform3D(Float3 p, Float3 euler)
+		{
+			position = p;
+			rotation = Quaternion(euler);
+			scale = Float3(1, 1, 1);
+		}
+
+		Transform3D(Float3 p, Quaternion q)
+		{
+			position = p;
+			rotation = q;
+			scale = Float3(1, 1, 1);
+		}
+
+		Transform3D(Float3 p, Float3 euler, Float3 s)
+		{
+			position = p;
+			rotation = Quaternion(euler);
+			scale = s;
+		}
+
+		Transform3D(Float3 p, Quaternion q, Float3 s)
+		{
+			position = p;
+			rotation = q;
+			scale = s;
+		}
+
+		OPERATOR Transform3D &operator=(Transform3D &otra)
+		{
+			position = otra.position;
+			rotation = otra.rotation;
+			scale = otra.scale;
+		}
+
+		OPERATOR bool operator==(Transform3D &otra)
+		{
+			return (position == otra.position) && (rotation == otra.rotation) && (scale == otra.scale);
+		}
+
+		// TODO: Euler angles
+
+		virtual void awake()
+		{
+			return;
+		}
+		virtual void start()
+		{
+			return;
+		}
+		virtual void update()
+		{
+			return;
+		}
+
+		virtual void fixedUpdate()
+		{
+			return;
+		}
+		virtual void onTriggerEnter()
+		{
+			return;
+		}
+		virtual void onTriggerStay()
+		{
+			return;
+		}
+		virtual void onTriggerExit()
+		{
+			return;
+		}
 	}
 };
 
