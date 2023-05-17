ed19677ea9ba5d99c00c133453c60ec2a39718d3
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-05-17 15:41:58 +0000

    Added a most of joint classes
    Added a most of the Joint2D classes, FrictionJoint2D, GearJoint2D, MotorJoint2D, and PrismaticJoint2D.

--- include/physics/joints2D.hpp
+++ include/physics/joints2D.hpp
@@ -32,11 +32,17 @@ namespace engine
 		{
 			internals::physics::Joint2DTuple tuple;
 			JointType2D type;
+			void *body2D;
+
 			Rigidbody2D *a;
 			Rigidbody2D *b;
+
 			bool collide;
+
+			Vec2 anchorA;
+			Vec2 anchorB;
 			public:
-			void init();
+			virtual void init();
 
 			void setType();
 			JointType2D getType();
@@ -47,6 +53,11 @@ namespace engine
 			const Rigidbody2D *getBodyB() const;
 			void setBodyA(Rigidbody2D &bodyA);
 			void setBodyB(Rigidbody2D &bodyB);
+
+			virtual Vec2 getAnchorA() const = 0;
+			virtual Vec2 getAnchorB() const = 0;
+			virtual void setAnchorA(Vec2 a) = 0;
+			virtual void setAnchorB(vec2 b) = 0;
 
 			Vec2 getReactionForce();
 			float getReactionTorque();
@@ -67,10 +78,12 @@ namespace engine
 			float stiffness;
 			float damping;
 			public:
-			void setAnchorA(Vec2 anchor);
-			void setAnchorB(Vec2 anchor);
-			Vec2 getAnchorA() const;
-			Vec2 getAnchorb() const;
+			void init() override;
+
+			void setAnchorA(Vec2 anchor) override;
+			void setAnchorB(Vec2 anchor) override;
+			Vec2 getAnchorA() const override;
+			Vec2 getAnchorb() const override;
 
 			void setLength(float f);
 			float getLength() const;
@@ -86,7 +99,144 @@ namespace engine
 			void setDamping(float f);
 			float getDamping() const;
 		};
+
+		class FrictionJoint2D : Joint2D
+		{
+			Vec2 anchorA;
+			Vec2 anchorB;
+
+			float maxFriction;
+			float maxTorque;
+			public:
+			void init() override;
+
+			void setAnchorA(Vec2 anchor) override;
+			void setAnchorB(Vec2 anchor) override;
+			Vec2 getAnchorA() const override;
+			Vec2 getAnchorb() const override;
+
+			void setMaxForce(float f);
+			float getMaxForce() const;
+
+			void setMaxTorque(float f);
+			float getMaxTorque() const;
+		};
+
+		class GearJoint2D : Joint2D
+		{
+			Vec2 anchorA;
+			Vec2 anchorB;
+
+			float ratio;
+			Joint2D *jointA;
+			Joint2D *jointB;
+			public:
+			void init() override;
+
+			void setAnchorA(Vec2 anchor) override;
+			void setAnchorB(Vec2 anchor) override;
+			Vec2 getAnchorA() const override;
+			Vec2 getAnchorb() const override;
+
+			Vec2 getReactionForce() const;
+			float getReactionTorque() const;
+
+			void setRatio(float r);
+			float getRatio() const;
+		};
+
+		class MotorJoint2D : Joint2D
+		{
+			Vec2 anchorA;
+			Vec2 anchorB;
+
+			float maxForce;
+			float maxTorque;
+			float correctionFactor;
+			float angularOffset;
+			Vec2 linearOffset;
+			public:
+			void init() override;
+
+			void setAnchorA(Vec2 a) override;
+			void setAnchorB(Vec2 a) override;
+			Vec2 getAnchorA() override const;
+			Vec2 getAnchorb() override const;
+
+			void setLinearOffset(Vec2 v);
+			Vec2 getLinearOffset() const;
+
+			void setAngularOffset(float o);
+			float getAngularOffset() const;
+
+			Vec2 getReactionForce() const;
+			float getReactionTorque() const;
+
+			void setMaxForce(float f);
+			float getMaxForce() const;
+
+			void setMaxTorque(float f);
+			float getMaxTorque() const;
+
+			void setCorrectionFactor(float f);
+			float getCorrectionFactor() const;
+		}
+
+		class PrismaticJoint2D : Joint2D
+		{
+			Vec2 anchorA;
+			Vec2 anchorB;
+
+			float referenceAngle;
+			bool enableLimit;
+			float lowerTranslation;
+			float upperTranslation;
+			bool enableMotor;
+			float maxMotorForce;
+			float motorSpeed;
+			public:
+			void init() override;
+
+			void setAnchorA(Vec2 a) override;
+			void setAnchorB(Vec2 a) override;
+			Vec2 getAnchorA() override const;
+			Vec2 getAnchorb() override const;
+
+			Vec2 getReactionForce() const;
+			float getReactionTorque() const;
+
+			Vec2 getAxisA() const;
+
+			void setReferenceAngle(float a);
+			float getReferenceAngle() const;
+
+			void setJointLowerTranslationLimit(float l);
+			void setJointUpperTranslationLimit(float u);
+
+			float getJointTranslation() const;
+			
+			float getJointSpeed() const;
+			
+			bool isLimitEnabled() const;
+			void enableLimit();
+			void disableLimit();
+
+			float getLowerLimit() const;
+			float getUpperLimit() const;
+			void setLimits(float upper, float lower);
+
+			bool isMotorEnabled() const;
+			void enableMotor() const;
+			void disableMotor() const;
+
+			void setMotorSpeed(float s);
+			float getMotorSpeed() const;
+
+			float getMaxMotorForce() const;
+			void setMaxMotorForce(float f);
+			float getMotorForce(float n) const;
+		};
 	}
 }
 
-#endif	
+#endif
