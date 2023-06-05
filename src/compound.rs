e6ea08fdaa5d29e3a20843c7576b74679851d911
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-06-05 12:39:48 +0000

    Added glm
    Added glm stuff.

---
+++ src/compound.rs
@@ -0,0 +1,10 @@
+/*s
+*   name: src/compound.rs
+*   origin: Citrus Engine
+*   purpose: Provide GLM stuff
+*   author: https://github.com/ComradeYellowCitrusFruit
+*   license: LGPL-3.0-only
+*/
+
+pub use glm::*;
+pub use glm::builtin::*;
