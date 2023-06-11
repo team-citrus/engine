f679d02207ffaa2da31a79b15ebad2a3a6b12c88
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-06-11 19:00:35 -0400

    Effectively rewrote sound
    Rewrote all code that involved mixers to use Soloud audio buses. Deleted most of the Sound code.

--- src/sound/sound.rs
+++ src/sound/sound.rs
@@ -12,33 +12,47 @@ use std::{string::String, ffi::{c_void, c_char}, ops::DerefMut};
 use crate::{internal::vendor::__citrus_engine_vendor_file_to_slice, Vec3};
 
 pub struct Mixer {
-    owner: Option<&Mixer>,
-    sub: Vec<Mixer>,
     sounds: Vec<Sound>,
-    pan: f32,
     volume: f32,
-    position: Vec3<f32>,
+    int_repr: audio::Bus,
 }
 
 impl Mixer {
     pub fn default() -> Mixer {
-        Mixer { owner: None, sub: Vec::with_capacity(1), sounds: Vec::with_capacity(8), pan: 0, volume: 100, position: vec3(0, 0, 0) }
+        Mixer { sounds: Vec<Sound>::with_capacity(8), volume: 1.0, int_repr: audio::Bus::default() }
     }
 
-    pub fn new(pan: f32, volume: f32, position: Vec3<f32>) -> Mixer {
-        Mixer { owner: None, sub: Vec::with_capacity(1), sounds: Vec::with_capacity(8), pan: pan, volume: volume, position: position }
+    pub fn set_volume(&mut self, volume: f32) -> () {
+        self.volume = volume;
+        self.int_repr.set_volume(volume);
     }
 
-    pub fn default_child(&mut self) -> Mixer {
-        let child = Mixer { owner: Some(&self as &Mixer), sub: Vec::with_capacity(1), sounds: Vec::with_capacity(8), pan: 0, volume: 100, position: vec3(0, 0, 0) };
-        self.sub.push(child);
-        child
+    pub fn get_volume(&self) -> f32 {
+        self.volume
     }
 
-    pub fn new_child(pan: f32, volume: f32, position: Vec3<f32>) -> Mixer {
-        let child = Mixer { owner: Some(&self as &Mixer), sub: Vec::with_capacity(1), sounds: Vec::with_capacity(8), pan: pan, volume: volume, position: position };
-        self.sub.push(child);
-        child
+    pub fn default_sound(&mut self, filename: String) -> &Sound {
+       let tmp = Sound { file: filename, handle: unsafe { Handle::from_raw(0) }, is3d: false, pan: 0.0, volume: 1.0, position: vec3(0.0, 0.0, 0.0), parent: Some(&*self) };
+       self.sounds.push(tmp);
+       self.sounds.last().unwrap()
+    }
+
+    pub fn new_sound(&mut self, filename: String, pan: f32, volume: f32) -> &Sound {
+        let tmp = Sound { file: filename, handle: unsafe { Handle::from_raw(0) }, is3d: false, pan: pan, volume: volume, position: vec3(0.0, 0.0, 0.0), parent: Some(&*self) };
+        self.sounds.push(tmp);
+        self.sounds.last().unwrap()
+    }
+
+    pub fn default_sound_3d(&mut self, filename: String, position: Vec3<f32>) -> &Sound {
+        let tmp = Sound { file: filename, handle: unsafe {  Handle::from_raw(0) }, is3d: true, pan: 0.0, volume: 1.0, position: position, parent: Some(&*self) };
+        self.sounds.push(tmp);
+        self.sounds.last().unwrap()
+    }
+
+    pub fn new_sound_3d(&mut self, filename: String, position: Vec3<f32>, volume: f32) -> &Sound {
+        let tmp = Sound { file: filename, handle: unsafe { Handle::from_raw(0) }, is3d: true, pan: 0.0, volume: volume, position: position, parent: Some(&*self) };
+        self.sounds.push(tmp);
+        self.sounds.last().unwrap()
     }
 
     // TODO: Stuff
@@ -56,53 +70,55 @@ pub struct Sound {
 
 impl Sound {
     pub fn default(filename: String) -> Sound {
-        Sound { file: filename, handle: unsafe { Handle::from_raw(0) }, is3d: false, pan: 0.0, volume: 0.0, position: vec3(0.0, 0.0, 0.0), parent: None }
+        Sound { file: filename, handle: unsafe { Handle::from_raw(0) }, is3d: false, pan: 0.0, volume: 1.0, position: vec3(0.0, 0.0, 0.0), parent: None }
     }
 
-    pub fn play(&mut self) {
+    pub fn new(filename: String, pan: f32, volume: f32) -> Sound {
+        Sound { file: filename, handle: unsafe { Handle::from_raw(0) }, is3d: false, pan: pan, volume: volume, position: vec3(0.0, 0.0, 0.0), parent: None }
+    }
+
+    pub fn default_3d(filename: String, position: Vec3<f32>) -> Sound {
+        Sound { file: filename, handle: unsafe {  Handle::from_raw(0) }, is3d: true, pan: 0.0, volume: 1.0, position: position, parent: None }
+    }
+
+    pub fn new_3d(filename: String, position: Vec3<f32>, volume: f32) -> Sound {
+        Sound { file: filename, handle: unsafe { Handle::from_raw(0) }, is3d: true, pan: 0.0, volume: volume, position: position, parent: None }
+    }
+
+    pub fn play(&mut self, start_paused: bool) {
         let mut wav = audio::Wav::default();
         wav.load_mem(__citrus_engine_vendor_file_to_slice(self.file.as_bytes().as_ptr() as *const c_char, 0)).unwrap();
 
         match audio_engine.lock() {
-            Ok(mut guard) => { self.handle = guard.deref_mut().play(&wav) },
+            Ok(mut guard) => {
+                if self.is3d {
+                    self.handle = match self.parent {
+                        Some(handle) => {
+                            let tmp = handle.int_repr.play_3d_ex(&wav, self.position.x, self.position.y, self.position.z, 0.0, 0.0, 0.0, self.volume, start_paused);
+                            self.parent = Some(handle);
+                            tmp
+                        },
+                        None => {
+                            self.parent = None;
+                            guard.deref_mut().play_3d_ex(&wav, self.position.x, self.position.y, self.position.z, 0.0, 0.0, 0.0, self.volume, start_paused, unsafe { Handle::from_raw(0) })
+                        }
+                    };
+                } else {
+                    self.handle = match self.parent {
+                        Some(handle) => {
+                            let tmp = handle.play_ex(&wav, self.volume, self.pan, start_paused);
+                            self.parent = Some(handle);
+                            tmp
+                        },
+                        None => {
+                            self.parent = None;
+                            guard.deref_mut().play_ex(&wav, self.volume, self.pan, start_paused, unsafe { Handle::from_raw(0) })
+                        }
+                    };
+                }
+            },
             Err(err) => { panic!("The guard mutex of the audio engine has been corrupted. {}", err) }
         }
-    }
-
-    pub fn play_new_default(filename: String) -> Sound {
-        let mut sound = Sound { file: filename, handle: unsafe { Handle::from_raw(0) }, is3d: false, pan: 0.0, volume: 0.0, position: vec3(0.0, 0.0, 0.0), parent: None };
-        let mut wav = audio::Wav::default();
-        wav.load_mem(__citrus_engine_vendor_file_to_slice(filename.as_bytes().as_ptr() as *const c_char, 0)).unwrap();
-
-        match audio_engine.lock() {
-            Ok(mut guard) => { sound.handle = guard.deref_mut().play(&wav) },
-            Err(err) => { panic!("The guard mutex of the audio engine has been corrupted. {}", err) }
-        }
-
-        sound
-    }
-
-    pub fn play_delayed(&mut self, time: f64) {
-        let mut wav = audio::Wav::default();
-        wav.load_mem(__citrus_engine_vendor_file_to_slice(self.file.as_bytes().as_ptr() as *const c_char, 0)).unwrap();
-
-        match audio_engine.lock() {
-            Ok(mut guard) => { self.handle = guard.deref_mut().play_clocked(time, &wav) },
-            Err(err) => { panic!("The guard mutex of the audio engine has been corrupted. {}", err) }
-        }
-    }
-
-    pub fn play_delayed_new_default(filename: String, time: f64) -> Sound {
-        let mut sound = Sound { file: filename, handle: unsafe { Handle::from_raw(0) }, is3d: false, pan: 0.0, volume: 0.0, position: vec3(0.0, 0.0, 0.0), parent: None };
-        let mut wav = audio::Wav::default();
-        wav.load_mem(__citrus_engine_vendor_file_to_slice(filename.as_bytes().as_ptr() as *const c_char, 0)).unwrap();
-
-        match audio_engine.lock() {
-            Ok(mut guard) => { sound.handle = guard.deref_mut().play_clocked(time, &wav) },
-            Err(err) => { panic!("The guard mutex of the audio engine has been corrupted. {}", err) }
-        }
-
-        sound
     }
 
     pub fn pause(&mut self) -> () {
